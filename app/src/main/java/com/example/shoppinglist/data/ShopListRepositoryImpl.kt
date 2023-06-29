3a5bdb3386be878ef98acd40b969867d9c96a06f
VsevolodL27 <lavrenov.vv@edu.spbstu.ru>
2023-06-29 12:54:17 +0300

    Added MainViewModel.kt at presentation folder. Modified MainActivity.kt and ShopListRepositoryImpl.kt.

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -10,6 +10,13 @@ object ShopListRepositoryImpl : ShopListRepository {
 
     private var autoIncrementId = 0
 
+    init {
+        for (i in 0 until 10) {
+            val item = ShopItem("Name $i", i, true)
+            addShopItem(item)
+        }
+    }
+
     override fun getShopItem(shopItemId: Int): ShopItem {
         return shopList.find {
             it.id == shopItemId
