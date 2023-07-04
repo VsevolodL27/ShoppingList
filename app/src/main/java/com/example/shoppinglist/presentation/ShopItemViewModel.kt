2fedd7079ea099a32f48057e25cd9b4ac59fd39b
VsevolodL27 <lavrenov.vv@edu.spbstu.ru>
2023-07-04 11:05:49 +0300

    Added ShopItemViewModel.kt

---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewModel.kt
@@ -0,0 +1,63 @@
+package com.example.shoppinglist.presentation
+
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.data.ShopListRepositoryImpl
+import com.example.shoppinglist.domain.AddShopItemUseCase
+import com.example.shoppinglist.domain.EditShopItemUseCase
+import com.example.shoppinglist.domain.GetShopItemUseCase
+import com.example.shoppinglist.domain.ShopItem
+import java.lang.Exception
+
+class ShopItemViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    fun getShopItem(shopItemId: Int) {
+        val item = getShopItemUseCase.getShopItem(shopItemId)
+    }
+
+    fun addShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        if (validateInput(name, count)) {
+            addShopItemUseCase.addShopItem(ShopItem(name, count, true))
+        }
+    }
+
+    fun editShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        if (validateInput(name, count)) {
+            editShopItemUseCase.editShopItem(ShopItem(name, count, true))
+        }
+    }
+
+    private fun parseName(inputName: String?): String {
+        return inputName?.trim() ?: ""
+    }
+
+    private fun parseCount(inputCount: String?): Int {
+        return try {
+            inputCount?.trim()?.toInt() ?: 0
+        } catch (e: Exception) {
+            0
+        }
+    }
+
+    private fun validateInput(name: String, count: Int): Boolean {
+        var result = true
+        if (name.isBlank()) {
+            // TODO: show error input name
+            result = false
+        }
+        if (count <= 0) {
+            // TODO: show error input count
+            result = false
+        }
+        return result
+    }
+}
